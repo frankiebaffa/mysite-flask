098d70cdfcc4674ab3c4ff9f4645f12dae4d3746
frankiebaffa <frankiebaffa@gmail.com>
2018-06-03 19:05:08 -0400

    fix shell context

--- mysite/frankiebaffa.py
+++ mysite/frankiebaffa.py
@@ -7,4 +7,4 @@ from app.models import User, Article
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Article': Article, 'Post': Post,
-        'Project', Project}
+        'Project': Project}
