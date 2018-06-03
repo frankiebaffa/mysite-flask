6350c13dbf8afab694329307e88ee33290d09e4c
frankiebaffa <frankiebaffa@gmail.com>
2018-06-03 00:16:28 -0400

    add upload, login, logout to admin navbar

--- mysite/app/__init__.py
+++ mysite/app/__init__.py
@@ -6,9 +6,13 @@ from flask_nav.elements import Navbar, View
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 
 app = Flask(__name__)
+photos = UploadSet('photos', IMAGES)
 app.config.from_object(Config)
+app.config['UPLOADED_PHOTOS_DEST'] = 'app/static/img'
+configure_uploads(app, photos)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
@@ -26,6 +30,20 @@ def mynavbar():
         View('Blog', 'blog'),
         View('Articles', 'articles'),
         View('About', 'aboutme'))
+    nav.register_element('top', navconfig)
+    return navconfig
+
+@nav.navigation()
+def adminnavbar():
+    navconfig = Navbar(
+        'Frankie Baffa',
+        View('Home', 'index'),
+        View('Blog', 'blog'),
+        View('Articles', 'articles'),
+        View('About', 'aboutme'),
+        View('Manage', 'manage'),
+        View('Upload', 'upload'),
+        View('Logout', 'logout'))
     nav.register_element('top', navconfig)
     return navconfig
 
