59296d05f6714bd53bab5fa3e946d7e658cbe419
frankiebaffa <frankiebaffa@gmail.com>
2018-06-05 21:38:27 -0400

    add contact form and edit forms for each model

--- mysite/app/forms.py
+++ mysite/app/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import TextAreaField, validators
 from wtforms.validators import DataRequired
 
 # Create form for logging-in users
@@ -19,14 +20,40 @@ class ArticleCreateForm(FlaskForm):
 # Create form for creating new blog post
 class PostCreateForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
-    body = StringField('Body', validators=[DataRequired()])
     imageurl = StringField('Image File Name', validators=[DataRequired()])
+    body = TextAreaField('Body', validators=[DataRequired()])
     submit = SubmitField('Post')
 
 # Create form for creating new recent project
 class ProjectCreateForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
-    body = StringField('Body', validators=[DataRequired()])
     url = StringField('URL', validators=[DataRequired()])
     imageurl = StringField('Image File Name', validators=[DataRequired()])
+    body = TextAreaField('Body', validators=[DataRequired()])
     submit = SubmitField('Post')
+
+class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email Address', [validators.Required(), validators.Email()])
+    subject = StringField('Subject', validators=[DataRequired()])
+    message = TextAreaField("Message", validators=[DataRequired()])
+    submit = SubmitField('Send')
+
+class ArticleEditForm(FlaskForm):
+    newbody = StringField('Body', validators=[DataRequired()])
+    newurl = StringField('URL', validators=[DataRequired()])
+    newimageurl = StringField('Image File Name', validators=[DataRequired()])
+    submit = SubmitField('Update')
+
+class PostEditForm(FlaskForm):
+    newtitle = StringField('Title', validators=[DataRequired()])
+    newimageurl = StringField('Image File Name', validators=[DataRequired()])
+    newbody = TextAreaField('Body', validators=[DataRequired()])
+    submit = SubmitField('Update')
+
+class ProjectEditForm(FlaskForm):
+    newtitle = StringField('Title', validators=[DataRequired()])
+    newurl = StringField('URL', validators=[DataRequired()])
+    newimageurl = StringField('Image File Name', validators=[DataRequired()])
+    newbody = TextAreaField('Body', validators=[DataRequired()])
+    submit = SubmitField('Update')
