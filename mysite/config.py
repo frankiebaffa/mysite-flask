b5f00704f4c3fcd1371ae46eb6d94dfca5433665
frankiebaffa <frankiebaffa@gmail.com>
2018-06-05 21:37:00 -0400

    move secret_key to 'local_settings.py'

--- mysite/config.py
+++ mysite/config.py
@@ -3,11 +3,7 @@ import os
 # Assign the base directory based on where the main flask app resides
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Config(object):
-    # Create a secret key for CSRF
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Seed in dirt'
-
-    # Assign the location of the sqlite3 database
+class Config(object):# Assign the location of the sqlite3 database
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
