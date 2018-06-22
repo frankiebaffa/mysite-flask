16192d995f179f05eaa8204a6f267ecfa6e321af
frankiebaffa <frankiebaffa@gmail.com>
2018-06-22 07:58:41 -0400

    add placeholders form fields

--- mysite/app/forms.py
+++ mysite/app/forms.py
@@ -5,31 +5,31 @@ from wtforms.validators import DataRequired
 
 # Create form for logging-in users
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()], render_kw={"placeholder": " Username"})
+    password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": " Email"})
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
 # Create form for creating a new article
 class ArticleCreateForm(FlaskForm):
-    body = StringField('Body', validators=[DataRequired()])
-    url = StringField('URL', validators=[DataRequired()])
-    imageurl = StringField('Image File Name', validators=[DataRequired()])
+    body = StringField('Body', validators=[DataRequired()], render_kw={"placeholder": " Title"})
+    url = StringField('URL', validators=[DataRequired()], render_kw={"placeholder": " URL"})
+    imageurl = StringField('Image File Name', validators=[DataRequired()], render_kw={"placeholder": " Image File Name"})
     submit = SubmitField('Post')
 
 # Create form for creating new blog post
 class PostCreateForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    imageurl = StringField('Image File Name', validators=[DataRequired()])
-    body = TextAreaField('Body', validators=[DataRequired()])
+    title = StringField('Title', validators=[DataRequired()], render_kw={"placeholder": " Title"})
+    imageurl = StringField('Image File Name', validators=[DataRequired()], render_kw={"placeholder": " Image File Name"})
+    body = TextAreaField('Body', validators=[DataRequired()], render_kw={"placeholder": " Body"})
     submit = SubmitField('Post')
 
 # Create form for creating new recent project
 class ProjectCreateForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    url = StringField('URL', validators=[DataRequired()])
-    imageurl = StringField('Image File Name', validators=[DataRequired()])
-    body = TextAreaField('Body', validators=[DataRequired()])
+    title = StringField('Title', validators=[DataRequired()], render_kw={"placeholder": " Title"})
+    url = StringField('URL', validators=[DataRequired()], render_kw={"placeholder": " URL"})
+    imageurl = StringField('Image File Name', validators=[DataRequired()], render_kw={"placeholder": " Image File Name"})
+    body = TextAreaField('Body', validators=[DataRequired()], render_kw={"placeholder": " Body"})
     submit = SubmitField('Post')
 
 class ContactForm(FlaskForm):
@@ -43,28 +43,28 @@ class ContactForm(FlaskForm):
     submit = SubmitField('Send')
 
 class ArticleEditForm(FlaskForm):
-    newbody = StringField('Body', validators=[DataRequired()])
-    newurl = StringField('URL', validators=[DataRequired()])
-    newimageurl = StringField('Image File Name', validators=[DataRequired()])
+    newbody = StringField('Body', validators=[DataRequired()], render_kw={"placeholder": " Title"})
+    newurl = StringField('URL', validators=[DataRequired()], render_kw={"placeholder": " URL"})
+    newimageurl = StringField('Image File Name', validators=[DataRequired()], render_kw={"placeholder": " Image URL"})
     submit = SubmitField('Update')
 
 class PostEditForm(FlaskForm):
-    newtitle = StringField('Title', validators=[DataRequired()])
-    newimageurl = StringField('Image File Name', validators=[DataRequired()])
-    newbody = TextAreaField('Body', validators=[DataRequired()])
+    newtitle = StringField('Title', validators=[DataRequired()], render_kw={"placeholder": " Title"})
+    newimageurl = StringField('Image File Name', validators=[DataRequired()], render_kw={"placeholder": " Image URL"})
+    newbody = TextAreaField('Body', validators=[DataRequired()], render_kw={"placeholder": " Body"})
     submit = SubmitField('Update')
 
 class ProjectEditForm(FlaskForm):
-    newtitle = StringField('Title', validators=[DataRequired()])
-    newurl = StringField('URL', validators=[DataRequired()])
-    newimageurl = StringField('Image File Name', validators=[DataRequired()])
-    newbody = TextAreaField('Body', validators=[DataRequired()])
+    newtitle = StringField('Title', validators=[DataRequired()], render_kw={"placeholder": " Title"})
+    newurl = StringField('URL', validators=[DataRequired()], render_kw={"placeholder": " URL"})
+    newimageurl = StringField('Image File Name', validators=[DataRequired()], render_kw={"placeholder": " Image URL"})
+    newbody = TextAreaField('Body', validators=[DataRequired()], render_kw={"placeholder": " Body"})
     submit = SubmitField('Update')
 
 class AboutCreateForm(FlaskForm):
-    body = TextAreaField('Body', validators=[DataRequired()])
+    body = TextAreaField('Body', validators=[DataRequired()], render_kw={"placeholder": " Body Paragraph"})
     submit = SubmitField('Create')
 
 class AboutEditForm(FlaskForm):
-    newbody = TextAreaField('Body', validators=[DataRequired()])
+    newbody = TextAreaField('Body', validators=[DataRequired()], render_kw={"placeholder": " Body Paragraph"})
     submit = SubmitField('Update')
